4f5a51be339bf169b88b0a0d7caffe2b1638722d
saphyxia <210693787@qq.com>
2023-04-30 18:44:44 +0800

    lpf
    Former-commit-id: f2b3064768a5ee76ed01c269aa6e7f7608fb2768

--- Components/Algorithm/Inc/lowpass_filter.h
+++ Components/Algorithm/Inc/lowpass_filter.h
@@ -20,7 +20,26 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+/* Exported types ------------------------------------------------------------*/
+typedef struct
+{
+    float input;        //input value
+    float output;       //output value
+    float alpha;        //filter coefficient
+    float frame_period; //frame perood
+}lpf_first_order_t;
+
+typedef struct 
+{
+    float input;      //input value
+    float output[3];  //output value
+    float alpha[3];   //filter coefficient
+}lpf_second_order_t;
 
 /* Exported functions prototypes ---------------------------------------------*/
+extern void lpf_first_order_init(lpf_first_order_t *lpf,float alpha,float frame_period);
+extern float lpf_first_order_calc(lpf_first_order_t *lpf,float input);
+extern void lpf_second_order_init(lpf_second_order_t *lpf,float alpha[3]);
+extern float lpf_second_order_calc(lpf_second_order_t *lpf,float input);
 
 #endif //LOWPASS_FILTER_H
--- Components/Algorithm/Src/lowpass_filter.c
+++ Components/Algorithm/Src/lowpass_filter.c
@@ -15,5 +15,51 @@
 /* Includes ------------------------------------------------------------------*/
 #include "lowpass_filter.h"
 
+/**
+  * @brief first order low-pass filter init
+  * @retval none
+  */
+void lpf_first_order_init(lpf_first_order_t *lpf,float alpha,float frame_period)
+{
+  lpf->alpha = alpha;
+  lpf->frame_period = frame_period;
+  lpf->input = 0;
+  lpf->output = 0;
+}
+/**
+  * @brief first order low-pass filter calculate
+  * @retval filter output
+  */
+float lpf_first_order_calc(lpf_first_order_t *lpf,float input)
+{
+  lpf->input = input;
 
+  lpf->output = lpf->alpha/(lpf->alpha+lpf->frame_period)*lpf->output 
+              + lpf->frame_period/(lpf->alpha+lpf->frame_period)*lpf->input;
+
+  return lpf->output;
+}
+/**
+  * @brief second order low-pass filter init
+  * @retval none
+  */
+void lpf_second_order_init(lpf_second_order_t *lpf,float alpha[3])
+{
+  memcpy(lpf->alpha,alpha,sizeof(alpha));
+  lpf->input = 0;
+  memset(lpf->output,0,sizeof(lpf->output));
+}
+/**
+  * @brief second order low-pass filter calculate
+  * @retval filter output
+  */
+float lpf_second_order_calc(lpf_second_order_t *lpf,float input)
+{
+	lpf->input = input;
+	lpf->output[0] = lpf->output[1];
+	lpf->output[1] = lpf->output[2];
+  lpf->output[2] = lpf->alpha[0]*lpf->output[1] + lpf->alpha[1]*lpf->output[0] + lpf->alpha[2]*lpf->input;
+
+	return lpf->output[2];
+}
 
